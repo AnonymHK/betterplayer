26084d52758123e9a3c2de71ce769b9294edb3cf
Guy Luz <guy@remepy.com>
2023-07-16 16:19:41 +0300

    Moving to WakelockPlus since Wakelock is deprecated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,23 +9,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Shortcuts(
-        shortcuts: <LogicalKeySet, Intent>{
-          LogicalKeySet(LogicalKeyboardKey.select): const ActivateIntent(),
-        },
-        child: MaterialApp(
-          title: 'Better player demo',
-          localizationsDelegates: [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          supportedLocales: [
-            const Locale('en', 'US'),
-            const Locale('pl', 'PL'),
-          ],
-          theme: ThemeData(
-            primarySwatch: Colors.green,
-          ),
-          home: WelcomePage(),
-        ));
+      shortcuts: <LogicalKeySet, Intent>{
+        LogicalKeySet(LogicalKeyboardKey.select): const ActivateIntent(),
+      },
+      child: MaterialApp(
+        title: 'Better player demo',
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('en', 'US'),
+          const Locale('pl', 'PL'),
+        ],
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: WelcomePage(),
+      ),
+    );
   }
 }
